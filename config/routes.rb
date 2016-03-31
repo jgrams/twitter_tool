539e6498f1f85b5c47860dcfc0ff605d4f55266b
John Gramila <tynanjk@gmail.com>
2016-03-31 17:42:16 -0500

    Updated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'twitters/testing_page' => 'twitters#post_to_authorize'
   get 'new' => 'twitters#new_twitter_handle'
-  post 'new' => 'twitters#create_twitter_handle'
   get 'twitter_feeds' => 'twitters#show'
+  post 'twitter_feeds' => 'twitters#create_twitter_handle'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

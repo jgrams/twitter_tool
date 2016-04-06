97bf9ae9ede4b27e8e607890fd01a5613058fd6b
John Gramila <tynanjk@gmail.com>
2016-04-06 00:42:09 -0500

    Word cloud hash correctly formatted

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,11 @@
 class TweetsController < ApplicationController
-  def new
-  	current_user.twitter
+  def new_search
+    
+  	current_user.twitter.user_timeline( ,{count: 200, include_rts: true})
   end
 
   def show
+    #collect the most recent 200 tweets, returned as an array
     tweets = current_user.twitter.user_timeline({count: 200, include_rts: true})
     array_of_tweets = []
     tweets.each {|tweet| array_of_tweets.push(tweet[:text])}
@@ -16,7 +18,7 @@ class TweetsController < ApplicationController
     all_words = all_words.flatten
     #reduce the array of arrays created above into a hash with words as keys and counts as values
     all_words = all_words.reduce({}) { |h, w| h.update(w => h.fetch(w, 0) + 1) }
-    all_words.map{|key, value| Hash['text'=>key, 'weight'=>value]}
+    all_words.map{|key, value| Hash[text: key.to_s, weight: value.to_i]}
   end
 
 end

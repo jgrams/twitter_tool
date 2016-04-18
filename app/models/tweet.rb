58cc57a3db558ead0bb0ae83f73b4429a1869667
John Gramila <tynanjk@gmail.com>
2016-04-18 18:27:34 -0500

    Improved readability of code in model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,7 +7,7 @@ class Tweet < ActiveRecord::Base
     all_words = array_of_strings.map {|string| string.downcase.split(" ")}
     all_words = all_words.flatten
     #reduce the array of arrays created above into a hash with words as keys and counts as values
-    all_words = all_words.reduce({}) { |h, w| h.update(w => h.fetch(w, 0) + 1) }
+    all_words = all_words.reduce({}) { |memo, word| memo.update(word => memo.fetch(word, 0) + 1) }
     all_words.map{|key, value| Hash[text: key.to_s, weight: value.to_i]}
   end
 

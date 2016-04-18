fc1ec2df42ec20d2f8b8a404906d4ef5e1888cdc
John Gramila <tynanjk@gmail.com>
2016-04-18 18:43:04 -0500

    built tweeter_search controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,9 +4,19 @@ class TweetsController < ApplicationController
 
   def show
     #collect the most recent 200 tweets, returned as an array
-    tweets = current_user.twitter.user_timeline({count: 200, include_rts: true})
+    tweets = current_user.twitter.user_timeline({count: 200, include_rts: true, trim_user: true})
     array_of_tweets = []
-    #adds the tweet field to 
+    #returns an array of tweet texts with identifying data removed
+    tweets.each {|tweet| array_of_tweets.push(tweet[:text])}
+    @word_count = reduce(array_of_tweets)
+  end
+
+  def tweeter_search
+    #collect the most recent 200 tweets, returned as an array
+    user = #collect params from form
+    tweets = current_user.twitter.user_timeline(user, {count: 200, include_rts: true, trim_user: true})
+    array_of_tweets = []
+    #returns an array of tweet texts with identifying data removed
     tweets.each {|tweet| array_of_tweets.push(tweet[:text])}
     @word_count = reduce(array_of_tweets)
   end

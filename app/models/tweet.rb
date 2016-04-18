621220a4fb50207b66a48ed4b24c0a5a3563d025
John Gramila <tynanjk@gmail.com>
2016-04-18 17:19:33 -0500

    Moved reduce function to Tweet Model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,14 @@
 class Tweet < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, :body, presence:true
+
+  def reduce(array_of_strings)
+    #return an array of arrays
+    all_words = array_of_strings.map {|string| string.downcase.split(" ")}
+    all_words = all_words.flatten
+    #reduce the array of arrays created above into a hash with words as keys and counts as values
+    all_words = all_words.reduce({}) { |h, w| h.update(w => h.fetch(w, 0) + 1) }
+    all_words.map{|key, value| Hash[text: key.to_s, weight: value.to_i]}
+  end
+
 end

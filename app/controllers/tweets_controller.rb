570f4fa8509c3ad60d7ab1af1ba04d1273865d35
John Gramila <tynanjk@gmail.com>
2016-04-18 16:22:29 -0500

    update development and test database in database.yml

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,7 @@ class TweetsController < ApplicationController
     #collect the most recent 200 tweets, returned as an array
     tweets = current_user.twitter.user_timeline({count: 200, include_rts: true})
     array_of_tweets = []
+    #adds the tweet field to 
     tweets.each {|tweet| array_of_tweets.push(tweet[:text])}
     @word_count = reduce(array_of_tweets)
   end

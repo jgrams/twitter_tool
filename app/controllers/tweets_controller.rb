05fbd341b955def7caecb7761e12a05f25f8b0ff
John Gramila <tynanjk@gmail.com>
2016-04-19 15:42:45 -0500

    Added searches controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,14 +17,6 @@ class TweetsController < ApplicationController
     @word_count = Tweet.reduce(array_of_tweets)
   end
 
-  def tweeter_search
-    #collect the most recent 200 tweets, returned as an array
-    user = 
-    tweets = current_user.twitter.user_timeline(user, {count: 200, include_rts: true, trim_user: true})
-    array_of_tweets = []
-    #returns an array of tweet texts with identifying data removed
-    tweets.each {|tweet| array_of_tweets.push(tweet[:text])}
-    @word_count = reduce(array_of_tweets)
-  end
+
 
 end

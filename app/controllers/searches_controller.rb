57555f14bd6332933cf4b5c429c14f45edea6228
John Gramila <tynanjk@gmail.com>
2016-04-21 01:06:43 -0500

    refactored how word_count_hash was created

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -14,15 +14,16 @@ class SearchesController < ApplicationController
   #copied from tweets controller
   def show
     #collect the most recent 200 tweets fo search is supplied, or ccureent user if no search supplied, returned as an array
-
     @search = current_user.searches.find_or_create_by(username: params[:username])
-    #
     tweets = current_user.twitter.user_timeline(@search.username||=current_user.handle, {count: 200, include_rts: true, trim_user: true})
     #I can simplify this code, move to model
     #make a giant string from the tweets in order to word count them
     string_of_tweets = ""
     tweets.each { |tweet| string_of_tweets << (tweet.text + " ") }
+    #save the recieved word_count_hash into the database
     @search.word_count =  Search.reduce(string_of_tweets)
+    #return an array for displaying on page
+    @sorted_word_count = @search.word_count.sort_by { |word, count| count }.reverse
     #makes a new search object that can be passed along to the search controller
     @new_search = current_user.searches.new
   end
--- app/models/search.rb
+++ app/models/search.rb
@@ -4,7 +4,7 @@ class Search < ActiveRecord::Base
 
   def self.reduce(long_tweet_string)
     #sanetize input with a regex and downcase and make an array of individual words
-    all_words = long_tweet_string.gsub(/[^0-9a-z@# ]/i, '').downcase.split ' '
+    all_words = long_tweet_string.gsub(/[^0-9a-z@# ]/i, '').downcase.split(' ')
     #reduce the array of arrays created above into a hash with words as keys and counts as values
     word_count_hash = all_words.reduce({}) { |memo, word| memo.update(word => memo.fetch(word, 0) + 1) }
   word_count_hash

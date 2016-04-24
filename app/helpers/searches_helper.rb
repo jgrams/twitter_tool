44068cad776191f12cb7320496319de880245100
John Gramila <tynanjk@gmail.com>
2016-04-24 01:44:18 -0500

    Major overhaul of css to understand where the circles are heriting from

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,6 +2,6 @@ module SearchesHelper
   #take an array of arrays(sort_by performed on hashes with word count values)
   #will transform into circle radius and return an integer, so maps to 20-100 rather than from 0
   def scale_range_to_percentage(object, min, max)
-    ((object[1].to_f-min)/(max-min) * (100-20) + 20).to_i.to_s.concat('px')
+    ((object[1].to_f-min)/(max-min) * (100-20) + 20).to_i.to_s.concat('%')
   end
 end

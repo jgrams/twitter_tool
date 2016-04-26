30fda54714d90e6a26fbc09304b9ca96878f3b13
John Gramila <tynanjk@gmail.com>
2016-04-26 17:13:56 -0500

    Messing with stylesheet cgetting text centered

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,7 +1,18 @@
 module SearchesHelper
   #take an array of arrays(sort_by performed on hashes with word count values)
-  #will transform into circle radius and return an integer, so maps to 20-100 rather than from 0
-  def scale_range_to_percentage(object, min, max)
-    ((object[1].to_f-min)/(max-min) * (100-20) + 20).to_i.to_s.concat('%')
+  #returns a pixel size on a scale of 200 for the svg cicle to be built at, so maps to 40-400 rather than from 0
+  def scale_range_to_pixel_size(object, min, max)
+    ((object[1].to_f-min)/(max-min) * (200-60) + 60).to_i.to_s.concat('px')
   end
+
+  def nest_svg_divs(content, pixel_size)
+    
+    content_tag('div', style: ["width: #{pixel_size};", "height: #{pixel_size};"], class: "svg-wrapper") do
+      content_tag('svg', class: "svg-circle") do
+        content_tag('text', "#{content[0]}", class: "svg-text")
+      end
+    end
+  end
+
+
 end

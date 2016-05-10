85a2b819f173b835cba89c59b501da93cdba546c
John Gramila <tynanjk@gmail.com>
2016-05-10 10:47:44 -0500

    Centered text in svg

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -6,10 +6,10 @@ module SearchesHelper
   end
   #take an item in an array of arrays(sort_by performed on hashes with word count values)
   #returns a string of pixel size in format "Xpx" for the svg cicle to be built at, 
-  #so maps to "60-200px" from whatever word count range there is
+  #so maps to "80-200px" from whatever word count range there is
   def scale_word_count_to_pixel_size(item, range_minimum, range_maximum)
     pixel_maximum = 200
-    pixel_minimum = 60
+    pixel_minimum = 80
     ((item[1].to_f-range_minimum)/(range_maximum-range_minimum) * (pixel_maximum-pixel_minimum) + pixel_minimum).to_i.to_s.concat('px')
   end
 
@@ -18,7 +18,7 @@ module SearchesHelper
     pixel_size = scale_word_count_to_pixel_size(item, range_minimum, range_maximum)
     content_tag('div', style: ["width: #{pixel_size};", "height: #{pixel_size};"], class: "svg-wrapper") do
       content_tag('svg', class: "svg-circle") do
-        content_tag('text', "#{item[0]}", class: "svg-text")
+        content_tag('text', "#{item[0]}", class: "svg-text", x: "50%", y: "50%")
       end
     end
    end

404aebee11db2254ef845d258e1123bad04ad4fd
John Gramila <tynanjk@gmail.com>
2016-06-04 19:39:53 -0500

    style sheet edits

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -6,20 +6,19 @@ module SearchesHelper
   end
   #take an item in an array of arrays(sort_by performed on hashes with word count values)
   #returns a string of pixel size in format "Xpx" for the svg cicle to be built at, 
-  #so maps to "80-200px" from whatever word count range there is
+  #so maps to "80-300px" from whatever word count range there is
   def scale_word_count_to_pixel_size(item, range_minimum, range_maximum)
-    pixel_maximum = 200
+    pixel_maximum = 300
     pixel_minimum = 80
     ((item[1].to_f-range_minimum)/(range_maximum-range_minimum) * (pixel_maximum-pixel_minimum) + pixel_minimum).to_i.to_s.concat('px')
   end
 
-  #create divs to contain the svg and text elements
+  #create divs to contain the svg and text elements, curently passing in some style elements also
   def nest_svg_divs(item, range_minimum, range_maximum)
     pixel_size = scale_word_count_to_pixel_size(item, range_minimum, range_maximum)
-    content_tag('div', style: ["width: #{pixel_size};", "height: #{pixel_size};"], class: "svg-wrapper") do
-      content_tag('svg', class: "svg-circle") do
-        content_tag('text', "#{item[0]}", class: "svg-text", x: "50%", y: "50%")
-      end
+    content_tag('svg', style: ["width: #{pixel_size};", "height: #{pixel_size};"], class: "svg-wrapper") do
+      tag('circle', class: "svg-circle", r:'90%', cx: '50%', cy: '50%', stroke: 'dark-grey', fill:'#bdbdbd').concat(
+      content_tag('text', "#{item[0]}", class: "svg-text", x: "50%", y: "50%", lengthAdjust: "spacingAndGlyphs", textLength: "70%"))
     end
    end
 

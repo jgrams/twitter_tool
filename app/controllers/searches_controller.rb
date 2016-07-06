fc37abd53370430d21759be4a03a47dde5ebb3d5
John Gramila <tynanjk@gmail.com>
2016-07-06 13:47:51 -0500

    write get_tweets function and made searchs happen not through current user

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,12 +11,6 @@ class SearchesController < ApplicationController
     end
   end
 
-  def collect_with_max_id(collection=[], max_id=nil, &block)
-    response = yield(max_id)
-    response.each {|tweet| collection.push(tweet.text)}
-    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-  end
-
   def determine_search(search)
     if search.username
       search.username
@@ -27,29 +21,45 @@ class SearchesController < ApplicationController
     end
   end
 
+  def determine_empty(username)
+
+  end
+
+
+  def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = yield(max_id)
+    response.each {|tweet| collection.push(tweet.text)}
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
+
+  def get_tweets(username)
+    collect_with_max_id do |max_id|
+      begin
+      options = {count: 200, include_rts: true}
+      options[:max_id] = max_id unless max_id.nil?
+      #puts in a search or the logged in user's name to the twitter api
+      current_user.twitter.user_timeline(determine_search(username))
+      rescue
+        redirect_to search_timeout_path
+        return
+      end
+    end
+
   def show
     binding.pry
     string_of_tweets = ""
     #collect the most recent 200 tweets fo search is supplied, or current user if no search supplied, returned as an array
-    #if there is a username and it already has content, just look up the database version.
-    if current_user.searches.find_by(username: params[:username]).username  && !current_user.searches.find_by(username: params[:username]).at_tweet_count.empty?
-      search = current_user.searches.find_by(username: params[:username])
+    #if there is a username and it already has content, just look up the search saved in the database.
+    #I should add a field where I record the users who have searched and 
+    if Search.find_by(username: params[:username]).username  && !Search.find_by(username: params[:username]).at_tweet_count.empty?
+      search = Search.find_by(username: params[:username])
       @at_tweet_count = Search.sort_word_count(search.at_tweet_count)
       @content_count = Search.sort_word_count(search.word_count)
       @hashtag_count = Search.sort_word_count(search.hashtag_count)
+    #if there is no search that matches the username, we call on twitter
     else
       search = current_user.searches.find_or_create_by(username: params[:username])
-      reply = collect_with_max_id do |max_id|
-          begin
-          options = {count: 200, include_rts: true}
-          options[:max_id] = max_id unless max_id.nil?
-          #puts in a search or the logged in user's name to the twitter api
-          current_user.twitter.user_timeline(determine_search(search))
-          rescue
-            redirect_to search_timeout_path
-            return
-          end
-        end
+      get_tweets(search)
       #save the recieved word_count_hash into the database
       word_count_hash = Search.reduce(reply)
       #sanitize word_count from the model
@@ -72,7 +82,6 @@ class SearchesController < ApplicationController
 
   def fail
     @search = current_user.searches.new(search_params)
-    binding.pry
   end
 
 

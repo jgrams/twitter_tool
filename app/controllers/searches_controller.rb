3a322d63dae5914cd5a181cd18eb7db4942776a5
John Gramila <tynanjk@gmail.com>
2016-07-12 20:55:22 -0500

    user_show working

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,21 +3,16 @@ class SearchesController < ApplicationController
   def create
     @search = current_user.searches.new(search_params)
     if current_user.twitter.user(@search.username) && @search.save
-      binding.pry
       redirect_to search_show_path(search_params)
       else
       redirect_to search_fail_path(search_params)
     end
   end
 
-  def collect_with_max_id(collection=[], max_id=nil, &block)
-    redirect_to search_fail_path(search_params)
+  def collect_with_max_id(collection="", max_id=nil, &block)
     response = yield(max_id)
-    collection += response
-    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-  rescue => e
-    Rails.logger.error { "Error while fetching tweets, #{e.message} #{e.backtrace.join("\n")}" }
-    nil
+    response.each{|tweet| collection << (tweet.text + " ")}
+    response.empty? ? collection : collect_with_max_id(collection, response.last.id - 1, &block)
   end
 
   def get_tweets(username)
@@ -25,27 +20,21 @@ class SearchesController < ApplicationController
       options = {count: 200, include_rts: true}
       options[:max_id] = max_id unless max_id.nil?
       #puts in a search or the logged in user's name to the twitter api
-      current_user.twitter.user_timeline(search)
+      current_user.twitter.user_timeline(username, options)
     end
-  rescue => e
-    Rails.logger.error { "Error while fetching tweets for: #{username}, #{e.message} #{e.backtrace.join("\n")}" }
-    nil
-  end 
+  end
 
   #show the user's tweets
   def user_show
-    binding.pry
-    string_of_tweets = ""
-    if !Search.find_by(username: current_user.handle).word_count.empty?
+    if Search.find_by(username: current_user.handle)
       search = Search.find_by(username: current_user.handle)
       @at_tweet_count = Search.sort_word_count(search.at_tweet_count)
       @content_count = Search.sort_word_count(search.word_count)
       @hashtag_count = Search.sort_word_count(search.hashtag_count)
       @username = search.username
     else
-      search = current_user.searches.find_or_create_by username: current_user.handle
-      reply = get_tweets(current_user.handle)
-      binding.pry
+      search = current_user.searches.new username: current_user.handle
+      reply = get_tweets(search.username)
       #save the recieved word_count_hash into the database
       word_count_hash = Search.reduce(reply)
       #sanitize word_count from the model
@@ -67,11 +56,10 @@ class SearchesController < ApplicationController
   end
 
   def show
-    string_of_tweets = ""
     #collect the most recent 200 tweets fo search is supplied, or current user if no search supplied, returned as an array
     #if there is a username and it already has content, just look up the search saved in the database.
     #I should add a field where I record the users who have searched and 
-    if !Search.find_by(username: params[:username]).word_count.empty?
+    if Search.find_by(username: params[:username])
       search = Search.find_by(username: params[:username])
       @at_tweet_count = Search.sort_word_count(search.at_tweet_count)
       @content_count = Search.sort_word_count(search.word_count)
@@ -79,8 +67,8 @@ class SearchesController < ApplicationController
       @username = search.username
     #if there is no search that matches the username, we call on twitter
     else
-      search = current_user.searches.find_or_create_by username: (params[:username])
-      reply = get_tweets(search)
+      search = current_user.searches.new username: (params[:username])
+      reply = get_tweets(search.username)
       #save the recieved word_count_hash into the database
       word_count_hash = Search.reduce(reply)
       #sanitize word_count from the model

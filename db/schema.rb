5d897eed7a208bcd3823fc6889da68b91bb07219
John Gramila <tynanjk@gmail.com>
2016-12-24 13:19:31 -0600

    Updated database layout for tweets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113213049) do
+ActiveRecord::Schema.define(version: 20161224184604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +42,14 @@ ActiveRecord::Schema.define(version: 20161113213049) do
   end
 
   create_table "tweets", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "body"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
-    t.datetime "time"
+    t.string   "url"
+    t.datetime "twitter_created_at"
+    t.string   "text"
+    t.string   "sanetized_text"
+    t.string   "tweet_id"
   end
 
   create_table "users", force: :cascade do |t|
